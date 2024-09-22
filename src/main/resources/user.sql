c449991368e9adaac210b5faf5e0831f7eddb1fd
SharoneVerma <223018843@gehealthcare.com>
2024-09-23 00:51:20 +0530

    updating

--- src/main/resources/user.sql
+++ src/main/resources/user.sql
@@ -1,7 +1,1 @@
-CREATE TABLE IF NOT EXISTS users (
-                                     id SERIAL PRIMARY KEY,
-                                     username VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL
-    );
-
-INSERT INTO users (username, password) VALUES ('admin', 'admin123');
+INSERT INTO Login (userId, password) VALUES ('admin', 'admin123');
